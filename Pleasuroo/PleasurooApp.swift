f6b76e4494f7841ee20589a7318995b0aae12628
Valerii Zazulin <zazulinvalerii@gmail.com>
2024-11-13 16:06:10 +0100

    Initial Commit

---
+++ Pleasuroo/PleasurooApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleasurooApp.swift
+//  Pleasuroo
+//
+//  Created by Valery Zazulin on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PleasurooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
