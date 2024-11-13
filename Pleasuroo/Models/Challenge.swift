4d987b4d5cf57671c67930bbccf870e3d4c74978
Valerii Zazulin <zazulinvalerii@gmail.com>
2024-11-13 17:01:32 +0100

    Create Pleasure and Challenge models

---
+++ Pleasuroo/Models/Challenge.swift
@@ -0,0 +1,21 @@
+//
+//  Challenge.swift
+//  Pleasuroo
+//
+//  Created by Valery Zazulin on 13/11/24.
+//
+
+import SwiftUI
+
+struct Challenge {
+    
+    let level: Int
+    let name: String
+    let shortDescription: String
+    let description: String
+    let image: Image
+    
+    var isLocked: Bool
+    var isDoneAngelWay: Bool
+}
+
---
+++ Pleasuroo/Models/Pleasure.swift
@@ -0,0 +1,17 @@
+//
+//  Pleasure.swift
+//  Pleasuroo
+//
+//  Created by Valery Zazulin on 13/11/24.
+//
+
+import SwiftUI
+
+struct Pleasure {
+    
+    let name: String
+    let color: Color
+    let image: Image
+    let challenges: [Challenge]
+    
+}
