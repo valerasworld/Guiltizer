c0d58946ef3271208c7f467eaa29b3c3ab52dd1b
Valerii Zazulin <zazulinvalerii@gmail.com>
2024-11-21 17:16:11 +0100

    Create `DevilAngelView`. Minor design fixes.

---
+++ Pleasuroo/Views/DevilAngelView.swift
@@ -0,0 +1,18 @@
+//
+//  DevilAngelView.swift
+//  Pleasuroo
+//
+//  Created by Valery Zazulin on 21/11/24.
+//
+
+import SwiftUI
+
+struct DevilAngelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DevilAngelView()
+}
